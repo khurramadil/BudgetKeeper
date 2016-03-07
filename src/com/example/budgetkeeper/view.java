f2f21e1278efaaf66ef8fca337fb02cdbb4b8a95
Khurram <khurram.adil9@gmail.com>
2016-03-07 15:33:16 +0500

    declared an int abc in view.java

--- src/com/example/budgetkeeper/view.java
+++ src/com/example/budgetkeeper/view.java
@@ -35,6 +35,8 @@ import android.widget.Toast;
 public class view extends Activity implements OnClickListener,
 		OnItemSelectedListener {
 
+	int abc;
+
 	DatabaseHelper myDb;
 
 	Button green_button, blue_button, red_button, yellow_button, home_button,
