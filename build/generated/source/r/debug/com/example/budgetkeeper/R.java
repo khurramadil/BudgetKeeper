caa53624ba94709277f95e99475113bdb750fc28
Ahmad <abilalc1@gmail.com>
2016-04-12 13:35:08 +0500

    Testing 1

--- build/generated/source/r/debug/com/example/budgetkeeper/R.java
+++ build/generated/source/r/debug/com/example/budgetkeeper/R.java
@@ -118,34 +118,45 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int calendar=0x7f030001;
-        public static final int custom_dialog=0x7f030002;
-        public static final int editentry=0x7f030003;
-        public static final int graph=0x7f030004;
-        public static final int newentry=0x7f030005;
-        public static final int prompt=0x7f030006;
-        public static final int view=0x7f030007;
+        public static final int activity_main2=0x7f030001;
+        public static final int calendar=0x7f030002;
+        public static final int custom_dialog=0x7f030003;
+        public static final int editentry=0x7f030004;
+        public static final int graph=0x7f030005;
+        public static final int newentry=0x7f030006;
+        public static final int prompt=0x7f030007;
+        public static final int view=0x7f030008;
     }
     public static final class menu {
         public static final int main=0x7f070000;
     }
     public static final class string {
         public static final int action_settings=0x7f060000;
-        public static final int app_name=0x7f060001;
-        public static final int custom_dialog_text=0x7f060002;
-        public static final int date_selected=0x7f060003;
-        public static final int hello_world=0x7f060004;
-        public static final int select_month=0x7f060005;
-        public static final int select_year=0x7f060006;
-        public static final int title_activity_editentry=0x7f060007;
-        public static final int title_calendar=0x7f060008;
-        public static final int title_graph=0x7f060009;
-        public static final int title_new_entry=0x7f06000a;
-        public static final int title_view=0x7f06000b;
-        public static final int txt1=0x7f06000c;
-        public static final int txt2=0x7f06000d;
-        public static final int txt3=0x7f06000e;
-        public static final int txt4=0x7f06000f;
+        public static final int action_sign_in=0x7f060001;
+        public static final int action_sign_in_short=0x7f060002;
+        public static final int app_name=0x7f060003;
+        public static final int custom_dialog_text=0x7f060004;
+        public static final int date_selected=0x7f060005;
+        public static final int error_field_required=0x7f060006;
+        public static final int error_incorrect_password=0x7f060007;
+        public static final int error_invalid_email=0x7f060008;
+        public static final int error_invalid_password=0x7f060009;
+        public static final int hello_world=0x7f06000a;
+        public static final int permission_rationale=0x7f06000b;
+        public static final int prompt_email=0x7f06000c;
+        public static final int prompt_password=0x7f06000d;
+        public static final int select_month=0x7f06000e;
+        public static final int select_year=0x7f06000f;
+        public static final int title_activity_editentry=0x7f060010;
+        public static final int title_activity_login=0x7f060011;
+        public static final int title_calendar=0x7f060012;
+        public static final int title_graph=0x7f060013;
+        public static final int title_new_entry=0x7f060014;
+        public static final int title_view=0x7f060015;
+        public static final int txt1=0x7f060016;
+        public static final int txt2=0x7f060017;
+        public static final int txt3=0x7f060018;
+        public static final int txt4=0x7f060019;
     }
     public static final class style {
         /**  API 11 theme customizations can go here. 
